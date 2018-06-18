8a30743c6787f4c0978358375bd09bafc2c3fa86
btokraks22 <bree.v.v.13@gmail.com>
2018-06-18 21:23:16 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -5,6 +5,7 @@ time = "6pm"
 host_name = "Harry Potter"
 rsvp = "October 30"
 puts "Dear #{guest_name},"
+
 puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than #{rsvp}."
 puts "Sincerely,
 
