751898c32740f79c8032936285c47493a672c99f
btokraks22 <bree.v.v.13@gmail.com>
2018-06-18 21:27:11 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,9 +1,10 @@
-guest_name = "Ron Weasely"
-party_name = "Best Halloween Party"
-date = "October 31"
-time = "6pm" 
-host_name = "Harry Potter"
-rsvp = "October 30"
+# guest_name = "Ron Weasely"
+# party_name = "Best Halloween Party"
+# date = "October 31"
+# time = "6pm" 
+# host_name = "Harry Potter"
+# rsvp = "October 30"
+ def invitation
 puts "Dear #{guest_name},
 
 You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than #{rsvp}."
